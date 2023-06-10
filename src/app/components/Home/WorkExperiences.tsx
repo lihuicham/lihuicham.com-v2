52ecd3afab14d479693513faecb677b78ffb3ffc
Li Hui Cham <lihuicham@gmail.com>
2023-06-11 00:21:06 +0800

    add array

---
+++ src/app/components/Home/WorkExperiences.tsx
@@ -0,0 +1,35 @@
+interface Work {
+  role: string
+  company: string
+  link: string
+  type: string
+  period: string
+  description: string
+} 
+
+const works: Work[] = [
+  {
+    role: 'Software Engineer Intern',
+    company: 'Switcheo Labs',
+    link: 'https://www.switcheo.com',
+    type: 'Internship',
+    period: 'May 2023 - Nov 2023',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non lacus tincidunt lectus faucibus volutpat. Sed nec ex nec lorem semper placerat nec quis odio.',
+  },
+  {
+    role: 'Data Visualization Developer Intern',
+    company: 'The Straits Times',
+    link: 'https://www.straitstimes.com/multimedia/graphics',
+    type: 'Internship',
+    period: 'Dec 2022 - Jan 2023',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non lacus tincidunt lectus faucibus volutpat. Sed nec ex nec lorem semper placerat nec quis odio.',
+  },
+]
+
+export const WorkExperiences = () => {
+  return (
+    <div className="container mx-auto pt-16 flex flex-col items-center">
+      div
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
 import { Introduction } from "@/app/components/Home/Introduction"
 import { Tagline } from "@/app/components/Home/Tagline"
 import { AboutMe } from "@/app/components/Home/AboutMe"
+import { WorkExperiences } from "@/app/components/Home/WorkExperiences"
 
 export default function Home() {
   return (
     <div className="bg-primary-base space-y-32 md:space-y-56">
-      <div className="bg-primary-base flex-col space-y-48 md:space-y-56">
+      <div className="bg-primary-base space-y-48 md:space-y-56">
         <Introduction />
         <Tagline />
       </div>
-      <div className="bg-secondary-base pb-56">
+      <div className="bg-secondary-base pb-56 space-y-48 md:space-y-56">
         <AboutMe />
+        <WorkExperiences />
       </div>
     </div>
   )
